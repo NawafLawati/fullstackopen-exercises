81cabc610aceb506e91ad3159dc1a23fbd4b8cf3
Nawaf Al Lawati <nawaf.lawati@gmail.com>
2023-09-14 12:42:29 +0400

    finished part 1 and 2

---
+++ part1/part1_1and1_2/src/App.jsx
@@ -0,0 +1,51 @@
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  const Header = (props) => {
+    return (
+      <>
+      <h1>{props.course}</h1>
+      </>
+    )
+  }
+  
+  const Content = () => {
+    return (
+      <>
+         <Part title={part1} exercise={exercises1}/>
+         <Part title={part2} exercise={exercises2} />
+        <Part title={part3} exercise={exercises3} />
+      </>
+    )
+  }
+  
+  const Total = (props) => {
+    <>
+      <p>Number of exercises {props.exercise1 + props.exercise2 + props.exercise3}</p>
+    </>
+  }
+  
+  const Part = (props) => {
+    return (
+      <>
+      <p>{props.title} {props.exercise}</p>
+      </>
+    )
+  }
+
+  return (
+    <div>
+      <Header course={course}/>
+      <Content />
+      <Total exercise1={exercises1} exercise2={exercises2} exercise3={exercises3}/>    
+      </div>
+  )
+}
+
+export default App
