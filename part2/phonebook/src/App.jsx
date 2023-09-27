e2bd3c11e251fda5ad6f58ae25e226ddd151ae85
Nawaf Al Lawati <46934129+NawafLawati@users.noreply.github.com>
2023-09-28 00:26:18 +0400

    xdd

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
   useEffect(() => {
     console.log("effect");
     axios.get("http://localhost:3001/persons").then((response) => {
-      console.log("promise fulfilled");
+      console.log("promise fulfilled: xdd");
       setPersons(response.data);
     });
   }, []);
